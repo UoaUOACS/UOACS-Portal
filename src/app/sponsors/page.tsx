77ce8048563e6f3515c6f58dbc0f70ae7aee4db8
Tulza <40633228+tulza@users.noreply.github.com>
2024-09-12 00:53:31 +1200

    Update Sponsor Page

--- src/app/sponsors/page.tsx
+++ src/app/sponsors/page.tsx
@@ -10,13 +10,14 @@ export default function SponsorsPage() {
       <div className="flex flex-col min-h-dvh items-center px-2">
         <OtherPageHeader label="Our Sponsors!" />
         <motion.div
-          className="grid place-items-center"
+          className="grid place-items-center size-full flex-grow min-h-full"
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           transition={{ delay: 1 }}
         >
-          <div className="flex flex-col items-center">
-            <h3 className="text-3xl font-bold">V</h3>
+          <div className="flex flex-col items-center mb-8">
+            <h3 className="text-3xl font-bold mb-4">Silver Sponsors</h3>
+            <img src="assets/svg/sponsors/JaneStreetLogo.svg" className="w-[300px] max-h-[250px]" />
           </div>
         </motion.div>
       </div>
