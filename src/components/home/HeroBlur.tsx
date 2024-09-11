be4f739584519922717e3069dc6a8506db24de0e
Tulza <40633228+tulza@users.noreply.github.com>
2024-09-11 20:34:55 +1200

    animated dotted background

--- src/components/home/HeroBlur.tsx
+++ src/components/home/HeroBlur.tsx
@@ -1,10 +1,20 @@
+'use client';
+
+import { motion } from 'framer-motion';
+
 const HeroBlur = () => {
   return (
     <>
       <div className="bg-blur pointer-events-none absolute -z-10" />
-      {/* <div className="dottedmask  pointer-events-none grid place-items-center absolute size-full -z-10">
-        <div className="bg-white size-full"></div>
-      </div> */}
+      <div className="dottedmask brightness-200 pointer-events-none grid place-items-center absolute size-full -z-10">
+        <motion.div style={{ maskImage: 'radial-gradient(#fff, #fff0)' }} className="size-1/2">
+          <motion.div
+            className="grainy size-full "
+            animate={{ scale: [1, 1.3, 1] }}
+            transition={{ repeat: Infinity, duration: 5, ease: 'linear' }}
+          />
+        </motion.div>
+      </div>
     </>
   );
 };
