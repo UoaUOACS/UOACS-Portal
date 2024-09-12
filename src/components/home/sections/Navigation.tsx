4071dd961b8656f6fa5f60f1aef9c98636244be6
Tulza <40633228+tulza@users.noreply.github.com>
2024-09-12 16:01:33 +1200

    Add Sheet Menu

--- src/components/home/sections/Navigation.tsx
+++ src/components/home/sections/Navigation.tsx
@@ -1,34 +1,35 @@
 'use client';
 
+import { createContext, useContext, useState } from 'react';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import { motion, type Variants } from 'framer-motion';
-import { Ellipsis } from 'lucide-react';
+import { Ellipsis, X } from 'lucide-react';
 
 import { navigation } from '@/data/navigation';
 
 export const Navigation = () => {
   const pathname = usePathname();
   return (
-    <nav className=" my-4 w-full flex lg:items-center items-end flex-row sm:flex-col lg:flex-row justify-end  gap-4 text-lg  ">
-      <div className="w-16" />
-      <div className=" sm:gap-4 sm:flex hidden ">
-        {navigation.map(({ href, label }) => {
-          if (href === pathname) return;
-          return <NavigationLink href={href} label={label} key={href} />;
-        })}
-      </div>
-      <motion.a
-        whileHover={{ scale: 1.1 }}
-        href="https://forms.gle/kiLbz2N5WiqjxLoJ8"
-        className="bg-white text-black p-2 sm:px-8 px-12 rounded-full whitespace-nowrap select-none cursor-pointer"
-      >
-        Join us!
-      </motion.a>
-      <div className="size-12 bg-white flex sm:hidden items-center justify-center gap-1 flex-col rounded-full ">
-        <Ellipsis color="#000" />
-      </div>
-    </nav>
+    <SheetMenu>
+      <nav className=" my-4 w-full flex lg:items-center items-end flex-row sm:flex-col lg:flex-row justify-end  gap-4 text-lg  ">
+        <div className="w-16" />
+        <div className=" sm:gap-4 sm:flex hidden ">
+          {navigation.map(({ href, label }) => {
+            if (href === pathname) return;
+            return <NavigationLink href={href} label={label} key={href} />;
+          })}
+        </div>
+        <motion.a
+          whileHover={{ scale: 1.1 }}
+          href="https://forms.gle/kiLbz2N5WiqjxLoJ8"
+          className="bg-white text-black p-2 sm:px-8 px-12 rounded-full whitespace-nowrap select-none cursor-pointer"
+        >
+          Join us!
+        </motion.a>
+        <MenuButton />
+      </nav>
+    </SheetMenu>
   );
 };
 
@@ -63,5 +64,53 @@ export const NavigationLink = ({ href, label }: NavigationLinkProps) => {
         <p>{label}</p>
       </motion.div>
     </Link>
+  );
+};
+
+type MenuContextType = {
+  handleToggleMenu: () => void;
+};
+
+const MenuContext = createContext({} as MenuContextType);
+
+const useMenuSheet = () => {
+  const context = useContext(MenuContext);
+  if (!context) {
+    throw new Error('useMenuSheet must be used within a MenuProvider');
+  }
+  return context;
+};
+
+const SheetMenu = ({ children }: { children: React.ReactNode }) => {
+  const [open, setopen] = useState(false);
+  const handleToggleMenu = () => {
+    setopen(!open);
+  };
+  return (
+    <MenuContext.Provider value={{ handleToggleMenu }}>
+      <motion.div
+        className="fixed h-dvh w-dvw top-0 p-8 select-none bg-background z-[9999] "
+        initial={{ right: '-120%' }}
+        animate={{ right: open ? '0%' : '-120%' }}
+      >
+        <button onClick={handleToggleMenu}>
+          <X fill="#FFF" className="size-16" />
+        </button>
+      </motion.div>
+      {children}
+    </MenuContext.Provider>
+  );
+};
+
+const MenuButton = () => {
+  const { handleToggleMenu } = useMenuSheet();
+  return (
+    <motion.button
+      whileHover={{ scale: 1.1 }}
+      onClick={handleToggleMenu}
+      className="size-12 bg-white select-none flex sm:hidden items-center justify-center gap-1 flex-col rounded-full "
+    >
+      <Ellipsis color="#000" />
+    </motion.button>
   );
 };
