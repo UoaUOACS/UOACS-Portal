f21bf320d74906dff4b795b5a2e19aac52b9caee
Tulza <40633228+tulza@users.noreply.github.com>
2025-03-03 10:10:15 +1300

    Update signup url

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -153,11 +153,11 @@ const JoinUs = () => {
   return (
     <div className="relative flex flex-col place-items-center pb-24 sm:pb-16" id="Join us">
       <a
-        href="https://forms.gle/kiLbz2N5WiqjxLoJ8"
+        href="https://forms.gle/wE4YkHsPvyQ8Jx6YA"
         className="grid select-none place-items-center rounded-full border px-24 py-6 text-center text-4xl font-bold transition-colors duration-200 hover:bg-white hover:text-secondary sm:py-12 sm:text-5xl"
       >
         Join Us for <br />
-        2024!
+        2025!
       </a>
     </div>
   );
