3fead9ecd736edfd5c62c4721542d380413d73fb
Tulza <Tulza0410@gmail.com>
2025-05-04 00:45:24 +1200

    feat: update sponsors

--- src/app/sponsors/page.tsx
+++ src/app/sponsors/page.tsx
@@ -2,6 +2,13 @@ import OtherPageHeader from "@components/common/PageHeaderAnimation";
 import { FooterNavigation } from "@components/home/FooterNavigation";
 import * as motion from "framer-motion/client";
 
+import GrantThornton from "./assets/sponsors/gtlogo3.png";
+import JaneStreet from "./assets/sponsors/JaneStreetLogo.png";
+import Sandfield from "./assets/sponsors/Logo_Sandfield_GetanEdge_White.png";
+import Potentia from "./assets/sponsors/Potentia.svg";
+import UoA from "./assets/sponsors/UoA-Logo-Primary-RGB-Reversed-Small.png";
+import { Sponsor } from "./Sponsor";
+
 export default function SponsorsPage() {
   return (
     <>
@@ -14,15 +21,23 @@ export default function SponsorsPage() {
           transition={{ delay: 1 }}
         >
           <div className="relative mb-8 flex w-full flex-col items-center py-16">
-            <div className="absolute inset-1/2 -z-[9999] size-[150%] [translate:-50%_-50%]">
-              <Grainy />
+            <h3 className="mt-16 text-4xl font-bold text-blue-400">Diamond Sponsor</h3>
+            <div className="flex flex-col gap-16">
+              <Sponsor image={UoA} />
             </div>
-            <h3 className="mb-4 text-3xl font-bold">Silver Sponsors</h3>
-            <img
-              src="assets/svg/sponsors/JaneStreetLogo.svg"
-              className="max-h-[250px] min-w-[250px] max-w-[400px] bg-[radial-gradient(hsl(var(--background),1),hsl(var(--background),0.5),#fff0,#fff0)] p-8 transition-all hover:brightness-150 hover:saturate-150"
-              alt="Silver sponsor, Jane Street Logo"
-            />
+            <h3 className="mt-16 text-4xl font-bold text-yellow-400">Gold Sponsor</h3>
+            <div className="flex flex-col gap-16">
+              <Grainy />
+              <Sponsor image={Potentia} />
+            </div>
+            <h3 className="mt-16 text-4xl font-bold">Silver Sponsors</h3>
+            <div className="flex flex-col items-center">
+              <div className="flex flex-col sm:flex-row sm:gap-16">
+                <Sponsor image={JaneStreet} />
+                <Sponsor image={Sandfield} />
+              </div>
+              <Sponsor image={GrantThornton} />
+            </div>
           </div>
         </motion.div>
       </div>
@@ -34,13 +49,9 @@ export default function SponsorsPage() {
 const Grainy = () => {
   return (
     <div className="dottedmask pointer-events-none absolute inset-0 -z-10 grid size-full place-items-center brightness-200">
-      <motion.div style={{ maskImage: "radial-gradient(#fff,#fff, #fff0,#fff0)" }} className="size-2/3 sm:w-1/3">
-        <motion.div
-          className="grainy size-full"
-          animate={{ scale: [1, 1.3, 1] }}
-          transition={{ repeat: Infinity, duration: 5, ease: "linear" }}
-        />
-      </motion.div>
+      <div style={{ maskImage: "radial-gradient(#fff,#fff, #fff0,#fff0)" }} className="size-full">
+        <div className="grainy scale-animation size-full" />
+      </div>
     </div>
   );
 };
